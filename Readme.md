b694681805e0a8fdd1b293186fe7d2e8b62e3ceb
Georgian Stan <geeostan8@gmail.com>
2020-08-10 14:02:43 +0300

    first

---
+++ Readme.md
@@ -0,0 +1,5 @@
+## JWT Example
+
+This repo is a short example of JWT auth implemented using NodeJS.
+
+**!** This is to exemplify the process. To be used in production in this example the password should not be a simple text but should be a hash, and the token settings should be changed according to the requirements of the application.
---
+++ app.js
@@ -0,0 +1,74 @@
+/**
+ * * Dependencies
+ */
+const PORT = process.env.PORT || 3000;
+const bodyParser = require("body-parser");
+const app = require("express")();
+const jwt = require("jsonwebtoken");
+
+/**
+ * *vars
+ */
+
+const dbMock = [{ email: "test@gmail.com", pwd: "12345678" }];
+const secretKey = "shhhh";
+
+/**
+ * * auth
+ */
+const jwtGuard = () => {
+  return (req, res, next) => {
+    const { authorization } = req.headers;
+    if (!authorization) {
+      res.status(401).send();
+      return;
+    }
+    try {
+      const decodedToken = jwt.verify(authorization, secretKey);
+      const { email } = decodedToken;
+      const user = dbMock.find((user) => user.email === email);
+
+      if (!user) {
+        res.status(401).send();
+        return;
+      }
+    } catch (err) {
+      res.status(401).send();
+      return;
+    }
+
+    next();
+  };
+};
+
+/**
+ * * Express config and routes
+ */
+
+app.use(bodyParser());
+
+app.get("/", function (req, res) {
+  res.send();
+});
+
+app.post("/login", (req, res) => {
+  const { email, pwd } = req.body;
+
+  const user = dbMock.find(
+    (user) => (user.email === email) & (user.pwd === pwd)
+  );
+
+  const token = jwt.sign({ email: user.email }, secretKey, {
+    expiresIn: "1h",
+  });
+
+  res.send(token);
+});
+
+app.get("/protected", jwtGuard(), (req, res) => {
+  res.send("You are in");
+});
+
+app.listen(PORT, () => {
+  console.log(`App is up at port ${PORT}`);
+});
